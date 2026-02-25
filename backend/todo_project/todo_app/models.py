3c9d5ff74c294e08f49fc2dc3e294d23ba710423
nazerke <mnazerke89@gmail.com>
2026-02-25 12:24:05 +0500

    added bd settings,models

--- backend/todo_project/todo_app/models.py
+++ backend/todo_project/todo_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField(blank=True)
+    completed=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+
+class Course(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField(blank=True)
+    start_date=models.DateField(null=True,blank=True)
+    end_date=models.DateField(null=True,blank=True)
+    difficulty=models.CharField(max_length=50,blank=True)
+
+    def __str__(self):
+        return self.title
+
+class UserProgress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    progress = models.IntegerField(default=0)  # 0-100%
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('user', 'course')
--- backend/todo_project/todo_project/settings.py
+++ backend/todo_project/todo_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_manager',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
